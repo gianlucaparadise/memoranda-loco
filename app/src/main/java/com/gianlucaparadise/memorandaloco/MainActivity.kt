99bc111e761d521ddb270b1f4632cf2ee9e211d8
gianlucaparadise <gianlucaparadise@gmail.com>
2020-07-21 06:45:38 +0200

    Hilt basic setup

--- app/src/main/java/com/gianlucaparadise/memorandaloco/MainActivity.kt
+++ app/src/main/java/com/gianlucaparadise/memorandaloco/MainActivity.kt
@@ -3,7 +3,9 @@ package com.gianlucaparadise.memorandaloco
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.gianlucaparadise.memorandaloco.ui.main.MainFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/gianlucaparadise/memorandaloco/ui/main/MainFragment.kt
+++ app/src/main/java/com/gianlucaparadise/memorandaloco/ui/main/MainFragment.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.navGraphViewModels
 import com.gianlucaparadise.memorandaloco.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by navGraphViewModels(R.id.nav_graph) {
+        defaultViewModelProviderFactory
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +29,6 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
