8e23850237597b8e49668159312487656fbb0468
gianlucaparadise <gianlucaparadise@gmail.com>
2021-03-01 08:48:18 +0100

    Privacy Policy page handling

--- app/src/main/java/com/gianlucaparadise/memoloco/MainActivity.kt
+++ app/src/main/java/com/gianlucaparadise/memoloco/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.gianlucaparadise.memoloco
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.gianlucaparadise.memoloco.alert.AlertHelper
 import com.gianlucaparadise.memoloco.bluetooth.BluetoothHelper
@@ -28,6 +33,26 @@ class MainActivity : AppCompatActivity() {
         val notificationActionExtra =
             intent.getSerializableExtra(NotificationHelper.NOTIFICATION_ACTION_EXTRA) as? NotificationAction
         parseNotificationAction(notificationActionExtra)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_privacy_policy -> {
+                val url = getString(R.string.privacy_policy_link)
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(browserIntent)
+                true
+            }
+            else -> {
+                false
+            }
+        }
     }
 
     private fun parseNotificationAction(notificationAction: NotificationAction?) {
